236fae240a43722339a9a94e83a383a0c7d164c3
ksoha <soha.kulkarni09@gmail.com>
2026-08-19 15:37:05 +0530

    testing a basic TCP connection

--- cmd/redigo/main.go
+++ cmd/redigo/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+)
 
 func main() {
-	fmt.Println("Redigo is live")
+	//Open a TCP port and start listening for incoming connections
+
+	listener, err := net.Listen("tcp", ":6380")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer listener.Close()
+
+	fmt.Println("Redigo listening on port 6380")
+
+	//loop forever waiting for incoming clients to connect
+	//allow() itself blocks so the loop doesnt consume one million times
+	//each time someone connects, add them to handleconnection in a new goroutine
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("Error accepting connection:", err)
+			continue
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	fmt.Println("New client connected from", conn.RemoteAddr())
+
+	//just prove connection works
+	conn.Write([]byte("Welcome to Redigo!\n"))
 }
