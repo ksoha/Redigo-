c232cc6ec889e0cc6173026cd2a4fc51776b76ca
ksoha <soha.kulkarni09@gmail.com>
2026-09-17 12:23:43 +0530

    created function to accept hashValue as a Redis data type

--- internal/store/hash.go
+++ internal/store/hash.go
@@ -1,1 +1,19 @@
 package store
+
+//HashValue represents a Redis Hash
+//
+//A Hash contains multiple key-value pairs, under one Redis Key
+//
+//ex :
+//name -> "Soha"
+//age -> 21
+//city -> "Pune"
+
+type HashValue struct {
+	Fields map[string]string //Fields is a map that holds the key-value pairs of the hash
+}
+
+// Type returns the Redis data type
+func (h HashValue) Type() string {
+	return "hash"
+}
